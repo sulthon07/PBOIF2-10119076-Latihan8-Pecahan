7abf1294c21e3816ba4669f3cff8eeb00e90a472
ACER <ACER@DESKTOP-JMQ2UKH>
2020-10-17 17:29:43 +0700

    PBOIF2-10119076-Latihan8-Pecahan

---
+++ src/pboif2/pkg10119076/latihan8/pecahan/PBOIF210119076Latihan8Pecahan.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119076.latihan8.pecahan;
+
+/**
+ *
+ * @author ACER
+ * NAMA     : Sulthon Naufal Akmal
+ * KELAS    : IF2
+ * NIM      : 10119076
+ * Deskripsi Program : Pecahan
+ */
+public class PBOIF210119076Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      float f;
+      double d;
+      f = 1234567890.123456789F;
+      d = 1_234_567_890.123456789;
+      
+      System.out.println("Float : " + f);
+      System.out.println("Double : " + d);
+    }
+    
+}
